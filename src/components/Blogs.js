0331aa3bad9b83f59a21dc07209b35e7200c3191
Brenden Thomas <brenden.thomas17@gmail.com>
2022-08-11 16:55:23 -0500

    Update Blogs.js

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -8,7 +8,11 @@ const Blogs =() =>{
     const [blogs, setblogs] = useState([])
 
     const getBlogInfo =()=>{
-        fetch('https://globalmessageboardly.herokuapp.com/')
+        fetch('https://globalmessageboardly.herokuapp.com/', 
+       { headers: {
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+            }})
         .then((response)=>{
             return response.json()
         }).then((response)=>{
