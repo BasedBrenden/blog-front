5247eb208ac50d2eee12a05bbfece1414fe574b5
Brenden Thomas <brenden.thomas17@gmail.com>
2022-11-21 11:18:06 -0600

    Enabled Article Image and author display

--- src/components/Article.js
+++ src/components/Article.js
@@ -39,26 +39,29 @@ const Article =() =>{
                 }
                 )
             .catch(err => console.log(err))
-
     }
 
     useEffect(()=>{
         GetComments()
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[toggle])
+
     
     return(
         
+            
         <div className="mainContainer">
             <div className="nav">
                 <Nav />
             </div>
-            
+
             <div className="articleContainer">
                 <p className="article-title">{Article.state.temp.title}</p>
                 <div className="article-header">
-                    <p>Posted by <span>John Doe</span> on {dateFormat(Article.state.temp.date,"mmmm dS, yyyy")}</p>
+                    <p>Posted by <span>{Article.state.temp.author}</span> on {dateFormat(Article.state.temp.date,"mmmm dS, yyyy")}</p>
                 </div>
+                <img src={Article.state.temp.image} alt="img not found" className="blog-image"></img>
+                
                 <p>{Article.state.temp.blogPost}</p>
                 
                 <Link to="/"><button type="button">Go back!</button></Link>   
@@ -83,8 +86,6 @@ const Article =() =>{
                     <button type="button" onClick={()=> {SubmitComment()}} id="commentPost">Post Comment</button>
                 </div>
             </div>
-
-            
           </div>
         
     )
